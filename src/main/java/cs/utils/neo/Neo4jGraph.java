65e1afec71ae7f4abc5bff9b2a7937c1c7aa9f09
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2023-09-16 16:24:58 +0200

    connecting to gc17 server neo4j

--- src/main/java/cs/utils/neo/Neo4jGraph.java
+++ src/main/java/cs/utils/neo/Neo4jGraph.java
@@ -7,7 +7,9 @@ import java.util.List;
 import static org.neo4j.driver.Values.parameters;
 
 public class Neo4jGraph {
-    String SERVER_ROOT_URI = "bolt://10.92.0.34:7687";
+    //http://a256-gc1-17.srv.aau.dk:7474/browser/
+    //String SERVER_ROOT_URI = "bolt://10.92.0.34:7687";
+    String SERVER_ROOT_URI = "bolt://a256-gc1-17.srv.aau.dk:7687";
     String username = "neo4j";
     String password = "12345678";
     private final Driver driver;
