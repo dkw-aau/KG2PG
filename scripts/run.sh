3ae001182fe99e45c523bb47911b79e6556848d2
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2023-11-09 10:45:49 +0100

    100 k test

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,7 +9,7 @@ docker build . -t $image
 echo "Clearing cache"
 sync; echo 1 > /proc/sys/vm/drop_caches
 
-container=kg2pg_dbp22marMonAdd
+container=kg2pg_dbp22marMonAdd_100k
 
 echo "About to run docker container: ${container}"
 
